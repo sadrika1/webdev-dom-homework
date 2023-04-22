c3e8549dda13cf1a13ea5d919c63e36097ea5d6c
Arina Dianova <muffin_purper-gurk@mail.ru>
2023-04-22 20:44:58 +0300

    API4

--- api.js
+++ api.js
@@ -1,85 +1,63 @@
-const buttonElement = document.getElementById("button-form");
-const inputNameElement = document.getElementById("input-name");
-const textareaElement = document.getElementById("textarea-form");
-const commentListElement = document.getElementById("comment-list");
-// const addCommText = document.getElementById('addComments');
-// const addCommElement = document.getElementById('add-comm-text')
-
-import { renderComments } from "./render.js"; 
-
-export let comments = [];
-
-export const fetchAndRender = () => {
-  return fetch(
-    "https://webdev-hw-api.vercel.app/api/v1/dianova-arina/comments",
-    {
+export function getComments({token}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/v2/dianova-arina/comments", {
       method: "GET",
-    }
-  )
-    .then((response) => response.json())
-    .then((responseData) => {
-      const locale = "ru-RU";
-      let todayData = { day: "numeric", month: "numeric", year: "2-digit" };
-      let todayTime = { hour: "numeric", minute: "2-digit" };
-      let userDate = new Date();
-      comments = responseData.comments.map((comment) => {
-        return {
-          name: comment?.author?.name,
-          date: `${userDate.toLocaleDateString(locale,todayData)} ${userDate.toLocaleTimeString(locale, todayTime)}`,
-          text: comment.text,
-          likes: comment.likes,
-          isLiked: false,
-        };
-      });
-      renderComments();
-    });
-};
-
-export const newComment = () => {
-    const locale = "ru-RU";
-    let todayData = { day: "numeric", month: "numeric", year: "2-digit" };
-    let todayTime = { hour: "numeric", minute: "2-digit" };
-    let userDate = new Date();
-  
-    fetch("https://webdev-hw-api.vercel.app/api/v1/dianova-arina/comments", {
-      method: "POST",
-      body: JSON.stringify({
-        name: inputNameElement.value,
-        text: textareaElement.value,
-        date: `${userDate.toLocaleDateString(
-          locale,
-          todayData
-        )} ${userDate.toLocaleTimeString(locale, todayTime)}`,
-        likes: 0,
-        isLiked: false,
-      }),
+      headers: {
+        Authorization: token,
+      },
     })
       .then((response) => {
-        if (response.status === 500) {
-          return Promise.reject(new Error ("Сервер упал"));
-        } else if (response.status === 400) {
-          return Promise.reject(new Error ("Неправильный ввод"));
-        } else {
-          return fetchAndRender();
-        }
-      })
-      .then(() => {
-        buttonElement.disabled = false;
-        //buttonElement.textContent = "Написать";
-        inputNameElement.value = "";
-        textareaElement.value = "";
-      })
-      .catch((error) => {
-        buttonElement.disabled = false;
-        if (error.message === "Сервер упал") {
-          return newComment();
-        }
-        if (error.message === "Неправильный ввод") {
-          alert("Имя и комментарий должны быть не короче 3 символов");
-        } else {
-          alert("Кажется, у вас сломался интернет, попробуйте позже");
-        }
-      });
-  };
+      if (response.status === 401) {
+      throw new Error('Нет авторизации');
+      }
+      return response.json()
+    })
+};
   
-  
+export function postComments({token, text, name, date}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/v2/dianova-arina/comments", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          text,
+          date,
+          likes: 0,
+          isLiked: false,
+        }),
+        headers: {
+          Authorization: token,
+        },
+      }).then((response) => {
+        return response.json();
+      })
+    };
+
+export function loginUser({login, password}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+    login,
+    password
+    }),
+  }).then((response) => {
+    if(response.status === 400) {
+      throw new Error ('Неверный логин или пароль')
+    }
+    return response.json();
+  })
+};
+
+export function regUser({login, password, name}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+    login,
+    password,
+    name,
+    }),
+  }).then((response) => {
+    if(response.status === 400) {
+      throw new Error ('Такой пользователь уже существует')
+    }
+    return response.json();
+  })
+};
--- index.js
+++ index.js
@@ -1,49 +1,162 @@
-const buttonElement = document.getElementById("button-form");
-const inputNameElement = document.getElementById("input-name");
-const textareaElement = document.getElementById("textarea-form");
-const commentListElement = document.getElementById("comment-list");
-// const addCommText = document.getElementById('addComments');
-// const addCommElement = document.getElementById('add-comm-text')
+// const buttonElement = document.getElementById("button-form");
+// const inputNameElement = document.getElementById("input-name");
+// const textareaElement = document.getElementById("textarea-form");
+// const commentListElement = document.getElementById("comment-list");
 
-import { renderComments } from "./render.js";
-import { comments } from "./api.js";
-import { fetchAndRender } from "./api.js";
-import { newComment } from "./api.js";
+import { getComments, postComments } from "./api.js";
+import { renderLoginComponent } from "./login-components.js";
 
-fetchAndRender();
+let comments = [];
+let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+
+token = null;
+
+const fetchAndRender = () => {
+  return getComments({ token }).then((responseData) => {
+    const locale = "ru-RU";
+    let todayData = { day: "numeric", month: "numeric", year: "2-digit" };
+    let todayTime = { hour: "numeric", minute: "2-digit" };
+    let userDate = new Date();
+    comments = responseData.comments.map((comment) => {
+      return {
+        name: comment?.author?.name,
+        date: `${userDate.toLocaleDateString(
+          locale,
+          todayData
+        )} ${userDate.toLocaleTimeString(locale, todayTime)}`,
+        text: comment.text,
+        likes: comment.likes,
+        isLiked: false,
+      };
+    });
+    renderApp();
+  });
+};
+
+const renderApp = () => {
+  const appElement = document.getElementById("app");
+  const commentsHtml = comments
+    .map((comment, index) => {
+      return `<li class="comment">
+      <div class="comment-header">
+        <div>${comment.name}</div>
+        <div>${comment.date}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text" data-index='${index}'> ${comment.text}</div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">${comment.likes}</span>
+          <button data-index='${index}' class="like-button ${comment.paint}" ></button>
+        </div>
+      </div>
+    </li>`;
+    })
+    .join("");
+
+  const appHtml = `<main class="container">
+      <ul class="comments" id="comment-list">
+    <!--список в JS-->     
+    ${commentsHtml}   
+      </ul>  
+      <article class="add-form">
+        <input
+          id="input-name"
+          type="text"
+          class="add-form-name"
+          placeholder="Введите ваше имя"/>
+    <textarea
+          id="textarea-form"
+          type="textarea"
+          class="add-form-text"
+          placeholder="Введите ваш комментарий"
+          rows="4"></textarea>
+        <div class="add-form-row">
+          <button class="add-form-button" id="button-form">Написать</button>
+        </div>
+      </article>
+    </main>`;
+
+  appElement.innerHTML = appHtml;
+
+  if (!token) {
+    renderLoginComponent({appElement, setToken: (newToken) => {
+      token = newToken;
+    }, fetchAndRender,
+  })
+    return;
+  }
+
+  const buttonElement = document.getElementById("button-form");
+  const inputNameElement = document.getElementById("input-name");
+  const textareaElement = document.getElementById("textarea-form");
+
+  const newComment = () => {
+    const locale = "ru-RU";
+    let todayData = { day: "numeric", month: "numeric", year: "2-digit" };
+    let todayTime = { hour: "numeric", minute: "2-digit" };
+    let userDate = new Date();
+
+    postComments({
+      token,
+      name: inputNameElement.value,
+      text: textareaElement.value,
+      date: `${userDate.toLocaleDateString(
+        locale,
+        todayData
+      )} ${userDate.toLocaleTimeString(locale, todayTime)}`,
+    })
+      .then((response) => {
+        if (response.status === 500) {
+          return Promise.reject(new Error("Сервер упал"));
+        } else if (response.status === 400) {
+          return Promise.reject(new Error("Неправильный ввод"));
+        } else {
+          return fetchAndRender();
+        }
+      })
+      .then(() => {
+        buttonElement.disabled = false;
+        inputNameElement.value = "";
+        textareaElement.value = "";
+      })
+      .catch((error) => {
+        buttonElement.disabled = false;
+        if (error.message === "Сервер упал") {
+          return newComment();
+        }
+        if (error.message === "Неправильный ввод") {
+          alert("Имя и комментарий должны быть не короче 3 символов");
+        } else {
+          alert("Кажется, у вас сломался интернет, попробуйте позже");
+        }
+      });
+  };
+  buttonElement.addEventListener("click", newComment);
+  initEventListeners();
+};
+renderApp();
 
 // лайки
-export const initEventListeners = () => {
+const initEventListeners = () => {
   const likeButtonElements = document.querySelectorAll(".like-button");
 
   for (const likeButtonElement of likeButtonElements) {
-    const index = +(likeButtonElement.dataset.index);
+    const index = +likeButtonElement.dataset.index;
     likeButtonElement.addEventListener("click", () => {
       if (comments[index].isLiked === false) {
-        //comments[index].paint = "-active-like";
+        comments[index].paint = "-active-like";
         comments[index].likes += 1;
         comments[index].isLiked = true;
       } else {
-        //comments[index].paint = "";
+        comments[index].paint = "";
         comments[index].likes -= 1;
         comments[index].isLiked = false;
       }
-      renderComments();
-    });
-    answerEvent();
-  }
-};
-// ответ на комментарии при клике
-const answerEvent = () => {
-  const answerCommElements = document.querySelectorAll(".comment-text");
-  for (const answerCommElement of answerCommElements) {
-    answerCommElement.addEventListener("click", () => {
-      const index = answerCommElement.dataset.index;
-      textareaElement.value = `> ${comments[index].text} \n${comments[index].name}`;
+      renderApp();
     });
   }
 };
-renderComments();
+// fetchAndRender();
 initEventListeners();
-
-buttonElement.addEventListener("click", newComment);
